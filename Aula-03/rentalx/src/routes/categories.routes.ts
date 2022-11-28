6ec1c77a340fff09dee722f16364624b45251d18
Iann-rst <iannrst11@gmail.com>
2022-11-28 18:49:01 -0300

    feat: rota categoria (post e get)

---
+++ Aula-03/rentalx/src/routes/categories.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+
+import { CategoriesRepository } from "../repositories/CategoriesRespository";
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  categoriesRepository.create({ name, description });
+
+  return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const allCategories = categoriesRepository.list();
+
+  return response.status(200).json(allCategories);
+});
+
+export { categoriesRoutes };
