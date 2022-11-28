f427bc1adb8d1038f2836f21d07dd09405544f7f
Iann-rst <iannrst11@gmail.com>
2022-11-28 18:48:18 -0300

    feat: criar e listar as categorias

---
+++ Aula-03/rentalx/src/repositories/CategoriesRespository.ts
@@ -0,0 +1,33 @@
+import { Category } from "../model/Category";
+
+// DTO - Data transfer object
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category); // representa um insert dentro da tabela categories
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+}
+
+export { CategoriesRepository };
