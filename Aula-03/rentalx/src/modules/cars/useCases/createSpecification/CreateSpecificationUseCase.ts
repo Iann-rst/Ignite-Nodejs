2052bc642d4005b3f9e61509b83bfeabdd724709
Iann-rst <iannrst11@gmail.com>
2022-11-29 15:33:13 -0300

    feat: implementando o controller da  Specification

--- Aula-03/rentalx/src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ Aula-03/rentalx/src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,11 +1,11 @@
-import { ISpecificationRepository } from "../repositories/ISpecificationRepository";
+import { ISpecificationRepository } from "../../repositories/ISpecificationRepository";
 
 interface IRequest {
   name: string;
   description: string;
 }
 
-class CreateSpecificationService {
+class CreateSpecificationUseCase {
   constructor(private specificationsRepository: ISpecificationRepository) {
     //
   }
@@ -24,4 +24,4 @@ class CreateSpecificationService {
   }
 }
 
-export { CreateSpecificationService };
+export { CreateSpecificationUseCase };
---
+++ Aula-03/rentalx/src/modules/cars/useCases/createSpecification/index.ts
@@ -0,0 +1,14 @@
+import { SpecificationRepository } from "../../repositories/implementations/SpecificationRepository";
+import { CreateSpecificationController } from "./CreateSpecificationController";
+import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
+
+const specificationRepository = new SpecificationRepository();
+
+const createSpecificationUseCase = new CreateSpecificationUseCase(
+  specificationRepository
+);
+const createSpecificationController = new CreateSpecificationController(
+  createSpecificationUseCase
+);
+
+export { createSpecificationController };
