7684f705076a808071254baee674bee49c0121e3
Iann-rst <iannrst11@gmail.com>
2022-11-29 12:12:38 -0300

    feat: criar e procurar uma specification

---
+++ Aula-03/rentalx/src/modules/cars/repositories/SpecificationRepository.ts
@@ -0,0 +1,36 @@
+import { Specification } from "../model/Specification";
+import {
+  ICreateSpecificationDTO,
+  ISpecificationRepository,
+} from "./ISpecificationRepository";
+
+class SpecificationRepository implements ISpecificationRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  // Procurar se existe uma especificação com o nome passado
+  findByName(name: string): Specification {
+    const specification = this.specifications.find(
+      (specification) => specification.name === name
+    );
+
+    return specification;
+  }
+
+  // Criar uma especificação
+  create({ name, description }: ICreateSpecificationDTO): void {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+    this.specifications.push(specification);
+  }
+}
+
+export { SpecificationRepository };
