2bd3ca97a2a7891b91f3b3ee2b464f6d9be379fd
Iann-rst <iannrst11@gmail.com>
2022-12-03 19:07:17 -0300

    Refatorando o caso de uso de categoria

--- Aula-03/rentalx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ Aula-03/rentalx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -18,8 +18,10 @@ class CreateCategoryUseCase {
     // Constructor
   }
 
-  execute({ name, description }: IRequest): void {
-    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+  async execute({ name, description }: IRequest): Promise<void> {
+    const categoryAlreadyExists = await this.categoriesRepository.findByName(
+      name
+    );
 
     if (categoryAlreadyExists) {
       throw new Error("Category already exists!");
