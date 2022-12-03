4ca3de057788a60a70afa65fb407b286552e4531
Iann-rst <iannrst11@gmail.com>
2022-12-03 18:52:37 -0300

    feat: database. Alterar o repositório de categoria

--- Aula-03/rentalx/src/modules/cars/repositories/ICategoriesRepository.ts
+++ Aula-03/rentalx/src/modules/cars/repositories/ICategoriesRepository.ts
@@ -8,9 +8,9 @@ interface ICreateCategoryDTO {
 
 // Contrato de categorias.
 interface ICategoriesRepository {
-  findByName(name: string): Category;
-  list(): Category[];
-  create({ name, description }: ICreateCategoryDTO): void;
+  findByName(name: string): Promise<Category>;
+  list(): Promise<Category[]>;
+  create({ name, description }: ICreateCategoryDTO): Promise<void>;
 }
 
 export { ICategoriesRepository, ICreateCategoryDTO };
