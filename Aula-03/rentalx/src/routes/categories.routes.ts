0ac4a7fbc313664053c8c598e0e04624a6611691
Iann-rst <iannrst11@gmail.com>
2022-12-04 18:59:50 -0300

    refactor: importCategory com TSyringe

--- Aula-03/rentalx/src/routes/categories.routes.ts
+++ Aula-03/rentalx/src/routes/categories.routes.ts
@@ -2,8 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 
 import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
-// import createCategoryController from "../modules/cars/useCases/createCategory";
-import { importCategoryController } from "../modules/cars/useCases/importCategory";
+import { ImportCategoryController } from "../modules/cars/useCases/importCategory/ImportCategoryController";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
@@ -11,6 +10,7 @@ const categoriesRoutes = Router();
 const upload = multer({ dest: "./tmp" });
 
 const createCategoryController = new CreateCategoryController();
+const importCategoryController = new ImportCategoryController();
 
 categoriesRoutes.post("/", createCategoryController.handle);
 
@@ -18,8 +18,10 @@ categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
 });
 
-categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
-  return importCategoryController.handle(request, response);
-});
+categoriesRoutes.post(
+  "/import",
+  upload.single("file"),
+  importCategoryController.handle
+);
 
 export { categoriesRoutes };
