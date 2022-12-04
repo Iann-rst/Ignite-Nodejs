3643ccb7a7b36bb074360ff4b31f761b4c3d0fd2
Iann-rst <iannrst11@gmail.com>
2022-12-04 19:10:03 -0300

    refactor: injeção de dependência com TSyringe

--- Aula-03/rentalx/src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ Aula-03/rentalx/src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,14 +1,12 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
 class ListCategoriesController {
-  constructor(private listCategoriesUseCase: ListCategoriesUseCase) {
-    // Constructor
-  }
-
-  handle(request: Request, response: Response) {
-    const all = this.listCategoriesUseCase.execute();
+  async handle(request: Request, response: Response): Promise<Response> {
+    const listCategoriesUseCase = container.resolve(ListCategoriesUseCase);
+    const all = await listCategoriesUseCase.execute();
 
     return response.json(all);
   }
