c0add0acfd806d7b0915170cdc14b3d7a48c697d
Iann-rst <iannrst11@gmail.com>
2022-12-05 20:02:02 -0300

    feat: tratamento de exceções

--- Aula-03/rentalx/src/server.ts
+++ Aula-03/rentalx/src/server.ts
@@ -1,10 +1,11 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import swaggerUi from "swagger-ui-express";
-import "reflect-metadata";
 
+import "reflect-metadata";
+import "express-async-errors";
+import { AppError } from "./erros/AppError";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
-
 import "./shared/container";
 import "./database";
 
@@ -14,5 +15,18 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 app.listen(3333, () => console.log("Server is running!"));
