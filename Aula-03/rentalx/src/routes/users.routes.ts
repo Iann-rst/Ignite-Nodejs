757dd212a615617a903814c120b4da16d3126655
Iann-rst <iannrst11@gmail.com>
2022-12-05 10:59:24 -0300

    feat: rota para criar um usuário

---
+++ Aula-03/rentalx/src/routes/users.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+
+const createUserController = new CreateUserController();
+const usersRoutes = Router();
+
+usersRoutes.post("/", createUserController.handle);
+export { usersRoutes };
