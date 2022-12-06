eaf2d8ad4e6bcd31acfd6f3f2ca64dee5c91a5ad
Iann-rst <iannrst11@gmail.com>
2022-12-06 12:32:23 -0300

    feat: receber a img do avatar

--- Aula-03/rentalx/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ Aula-03/rentalx/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -4,11 +4,11 @@ import { container } from "tsyringe";
 import { UpdateUserAvatarUseCase } from "./UpdateUserAvatarUseCase";
 
 class UpdateUserAvatarController {
-  async handle(request: Request, response: Response) {
+  async handle(request: Request, response: Response): Promise<Response> {
     const { id } = request.user;
 
     // Receber arquivo
-    const avatar_file = null;
+    const avatar_file = request.file.filename;
 
     const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
     await updateUserAvatarUseCase.execute({ user_id: id, avatar_file });
