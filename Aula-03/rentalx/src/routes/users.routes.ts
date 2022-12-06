b44672a0019f20325a384a8e65d240d6e3785cce
Iann-rst <iannrst11@gmail.com>
2022-12-06 12:32:56 -0300

    feat: add rota para alterar a foto de avatar user

--- Aula-03/rentalx/src/routes/users.routes.ts
+++ Aula-03/rentalx/src/routes/users.routes.ts
@@ -1,9 +1,23 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 const usersRoutes = Router();
 
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+
 usersRoutes.post("/", createUserController.handle);
+
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  uploadAvatar.single("avatar"),
+  updateUserAvatarController.handle
+);
 export { usersRoutes };
