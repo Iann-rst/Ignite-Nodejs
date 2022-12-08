6c020c85fad39e866b950c3a0749c5fcfb27ed36
Iann-rst <iannrst11@gmail.com>
2022-12-08 18:20:44 -0300

    feat: buscando carro pela placa

--- Aula-03/rentalx/src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ Aula-03/rentalx/src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -14,7 +14,7 @@ class CarsRepositoryInMemory implements ICarsRepository {
     fine_amount,
     name,
     license_plate,
-  }: ICreateCarDTO): Promise<void> {
+  }: ICreateCarDTO): Promise<Car> {
     const car = new Car();
 
     Object.assign(car, {
@@ -27,6 +27,12 @@ class CarsRepositoryInMemory implements ICarsRepository {
       license_plate,
     });
     this.cars.push(car);
+    return car;
+  }
+
+  async findByLicensePlate(license_plate: string): Promise<Car> {
+    const car = this.cars.find((car) => car.license_plate === license_plate);
+    return car;
   }
 }
 
