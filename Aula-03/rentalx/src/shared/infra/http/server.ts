a227e099cefbf0f046ed1a41bb958ca57be37282
Iann-rst <iannrst11@gmail.com>
2022-12-09 19:34:50 -0300

    fix: conexão com banco de dados

--- Aula-03/rentalx/src/shared/infra/http/server.ts
+++ Aula-03/rentalx/src/shared/infra/http/server.ts
@@ -4,12 +4,14 @@ import swaggerUi from "swagger-ui-express";
 import "reflect-metadata";
 import "express-async-errors";
 import { AppError } from "@shared/errors/AppError";
+import createConnection from "@shared/infra/typeorm";
 
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
-import "@shared/container";
-import "@shared/infra/typeorm";
 
+import "@shared/container";
+
+createConnection();
 const app = express();
 app.use(express.json());
 
