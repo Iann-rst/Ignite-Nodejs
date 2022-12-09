f37da6b5e34cceeb4ae43eae0bfe0d5999cc0fdd
Iann-rst <iannrst11@gmail.com>
2022-12-09 16:51:39 -0300

    feat: Singleton

--- Aula-03/rentalx/src/shared/container/index.ts
+++ Aula-03/rentalx/src/shared/container/index.ts
@@ -2,8 +2,10 @@ import { container } from "tsyringe";
 
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { CarRepository } from "@modules/cars/infra/typeorm/repositories/CarRepository";
 import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
 import { SpecificationRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationRepository";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
 import { ISpecificationRepository } from "@modules/cars/repositories/ISpecificationRepository";
 
@@ -22,3 +24,5 @@ container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository
 );
+
+container.registerSingleton<ICarsRepository>("CarsRepository", CarRepository);
