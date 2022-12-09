32471941bf4b71e261db5aec4a01633b1336877d
Iann-rst <iannrst11@gmail.com>
2022-12-09 16:39:16 -0300

    feat: CarRepository usando TypeORM

--- Aula-03/rentalx/src/modules/cars/infra/typeorm/repositories/CarRepository.ts
+++ Aula-03/rentalx/src/modules/cars/infra/typeorm/repositories/CarRepository.ts
@@ -1,14 +1,42 @@
+import { Repository, getRepository } from "typeorm";
+
 import { ICreateCarDTO } from "@modules/cars/dtos/ICreateCarDTO";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 
 import { Car } from "../entities/Car";
 
 class CarRepository implements ICarsRepository {
-  async create(data: ICreateCarDTO): Promise<Car> {
-    throw new Error("Method not implemented.");
+  private repository: Repository<Car>;
+
+  constructor() {
+    this.repository = getRepository(Car);
   }
-  findByLicensePlate(license_plate: string): Promise<Car> {
-    throw new Error("Method not implemented.");
+  async create({
+    brand,
+    category_id,
+    daily_rate,
+    description,
+    fine_amount,
+    license_plate,
+    name,
+  }: ICreateCarDTO): Promise<Car> {
+    const car = this.repository.create({
+      brand,
+      category_id,
+      daily_rate,
+      description,
+      fine_amount,
+      license_plate,
+      name,
+    });
+
+    await this.repository.save(car);
+    return car;
+  }
+
+  async findByLicensePlate(license_plate: string): Promise<Car> {
+    const car = await this.repository.findOne({ license_plate });
+    return car;
   }
 }
 
