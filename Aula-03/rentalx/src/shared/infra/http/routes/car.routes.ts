39795de87a637fc65bd88ac8255a7907480dffb9
Iann-rst <iannrst11@gmail.com>
2022-12-10 18:52:42 -0300

    feat: autenticar e verificar se usuário é admin

--- Aula-03/rentalx/src/shared/infra/http/routes/car.routes.ts
+++ Aula-03/rentalx/src/shared/infra/http/routes/car.routes.ts
@@ -2,9 +2,17 @@ import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 
+import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+
 const createCarController = new CreateCarController();
 
 const carsRoutes = Router();
 
-carsRoutes.post("/", createCarController.handle);
+carsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarController.handle
+);
 export { carsRoutes };
