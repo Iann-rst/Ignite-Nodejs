639dab8759f227036cf865063afc742041175309
Iann-rst <iannrst11@gmail.com>
2022-12-12 14:42:29 -0300

    feat: Controller para listar carros disponíveis

---
+++ Aula-03/rentalx/src/modules/cars/useCases/listCars/ListCarsController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { ListCarsUseCase } from "./ListCarsUseCase";
+
+class ListCarsController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { brand, name, category_id } = request.query;
+
+    const listAvailableCarsUseCase = container.resolve(ListCarsUseCase);
+
+    const cars = await listAvailableCarsUseCase.execute({
+      brand: brand as string,
+      name: name as string,
+      category_id: category_id as string,
+    });
+
+    return response.json(cars);
+  }
+}
+
+export { ListCarsController };
