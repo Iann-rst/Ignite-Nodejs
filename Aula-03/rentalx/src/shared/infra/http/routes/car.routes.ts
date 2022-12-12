81bbbf2ca0362c175a0a5899e96d271c787156fa
Iann-rst <iannrst11@gmail.com>
2022-12-12 14:42:47 -0300

    feat: adc a rota de listagem de carros disponíveis

--- Aula-03/rentalx/src/shared/infra/http/routes/car.routes.ts
+++ Aula-03/rentalx/src/shared/infra/http/routes/car.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { ListCarsController } from "@modules/cars/useCases/listCars/ListCarsController";
 
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const createCarController = new CreateCarController();
+const listCarController = new ListCarsController();
 
 const carsRoutes = Router();
 
@@ -15,4 +17,6 @@ carsRoutes.post(
   ensureAdmin,
   createCarController.handle
 );
+
+carsRoutes.get("/available", listCarController.handle);
 export { carsRoutes };
