0a0322209792e70bc44483f65455b6955f21779f
Iann-rst <iannrst11@gmail.com>
2022-12-20 12:07:26 -0300

    feat: UseCase para cadastrar especificação carro

--- Aula-03/rentalx/src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
+++ Aula-03/rentalx/src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -1,6 +1,8 @@
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { ISpecificationRepository } from "@modules/cars/repositories/ISpecificationRepository";
 
 import { AppError } from "../../../../shared/errors/AppError";
 
@@ -9,17 +11,30 @@ interface IRequest {
   specifications_id: string[];
 }
 
+@injectable()
 class CreateCarSpecificationUseCase {
   constructor(
-    // @inject("CarsRepository")
-    private carsRepository: ICarsRepository
-  ) { }
-  async execute({ car_id, specifications_id }: IRequest): Promise<void> {
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository,
+
+    @inject("SpecificationRepository")
+    private specificationsRepository: ISpecificationRepository
+  ) {}
+  async execute({ car_id, specifications_id }: IRequest): Promise<Car> {
     const carExists = await this.carsRepository.findById(car_id);
 
     if (!carExists) {
       throw new AppError("Car does not exists!");
     }
+
+    const specifications = await this.specificationsRepository.findByIds(
+      specifications_id
+    );
+
+    carExists.specifications = specifications;
+
+    await this.carsRepository.create(carExists);
+    return carExists;
   }
 }
 
