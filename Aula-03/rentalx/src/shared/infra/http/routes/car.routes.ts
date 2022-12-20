adfd89686dfca0c5b2d0219abc7ddf733fc0cab2
Iann-rst <iannrst11@gmail.com>
2022-12-20 15:58:47 -0300

    feat: Rota para fazer upload das imagens do carro

--- Aula-03/rentalx/src/shared/infra/http/routes/car.routes.ts
+++ Aula-03/rentalx/src/shared/infra/http/routes/car.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "@config/upload";
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 import { ListCarsController } from "@modules/cars/useCases/listCars/ListCarsController";
+import { UploadCarImageController } from "@modules/cars/useCases/uploadCarImage/UploadCarImageController";
 
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
@@ -10,8 +13,11 @@ import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 const createCarController = new CreateCarController();
 const listCarController = new ListCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImageController = new UploadCarImageController();
 
 const carsRoutes = Router();
+
+const uploadCarImage = multer(uploadConfig.upload("./tmp/cars"));
 
 carsRoutes.post(
   "/",
@@ -29,4 +35,11 @@ carsRoutes.post(
   createCarSpecificationController.handle
 );
 
+carsRoutes.post(
+  "/images/:id",
+  ensureAuthenticated,
+  ensureAdmin,
+  uploadCarImage.array("images"),
+  uploadCarImageController.handle
+);
 export { carsRoutes };
