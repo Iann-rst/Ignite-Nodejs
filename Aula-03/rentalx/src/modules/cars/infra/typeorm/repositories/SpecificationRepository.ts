46d81c186e7188c31b815aba28649d7078b76ea3
Iann-rst <iannrst11@gmail.com>
2022-12-20 11:00:20 -0300

    feat: função de buscar especificação pelo id

--- Aula-03/rentalx/src/modules/cars/infra/typeorm/repositories/SpecificationRepository.ts
+++ Aula-03/rentalx/src/modules/cars/infra/typeorm/repositories/SpecificationRepository.ts
@@ -1,11 +1,10 @@
 import { getRepository, Repository } from "typeorm";
 
+import { Specification } from "@modules/cars/infra/typeorm/entities/Specification";
 import {
   ICreateSpecificationDTO,
   ISpecificationRepository,
 } from "@modules/cars/repositories/ISpecificationRepository";
-
-import { Specification } from "../entities/Specification";
 
 class SpecificationRepository implements ISpecificationRepository {
   private repository: Repository<Specification>;
@@ -14,21 +13,30 @@ class SpecificationRepository implements ISpecificationRepository {
     this.repository = getRepository(Specification);
   }
 
+  async findByIds(ids: string[]): Promise<Specification[]> {
+    const specifications = await this.repository.findByIds(ids);
+    return specifications;
+  }
+
   // Procurar se existe uma especificação com o nome passado
   async findByName(name: string): Promise<Specification> {
-    const specification = this.repository.findOne({ name });
+    const specification = await this.repository.findOne({ name });
 
     return specification;
   }
 
   // Criar uma especificação
-  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+  async create({
+    name,
+    description,
+  }: ICreateSpecificationDTO): Promise<Specification> {
     const specification = this.repository.create({
       description,
       name,
     });
 
     await this.repository.save(specification);
+    return specification;
   }
 }
 
