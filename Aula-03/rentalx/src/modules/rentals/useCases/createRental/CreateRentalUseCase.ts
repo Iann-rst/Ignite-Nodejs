51ea831e6c8fbfdaa2334d4f278d2e023867555e
Iann-rst <iannrst11@gmail.com>
2022-12-20 22:46:22 -0300

    feat: UseCase para criação do aluguel

---
+++ Aula-03/rentalx/src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -0,0 +1,72 @@
+import { inject, injectable } from "tsyringe";
+
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+
+import { AppError } from "../../../../shared/errors/AppError";
+/**
+ * UseCase para fazer o aluguel de um carro
+ */
+
+interface IRequest {
+  user_id: string;
+  car_id: string;
+  expected_return_date: Date;
+}
+
+@injectable()
+class CreateRentalUseCase {
+  constructor(
+    @inject("RentalsRepository")
+    private rentalsRepository: IRentalsRepository,
+
+    @inject("DayjsDateProvider")
+    private dateProvider: IDateProvider
+  ) {}
+  async execute({
+    car_id,
+    expected_return_date,
+    user_id,
+  }: IRequest): Promise<Rental> {
+    const minimumHours = 24;
+
+    // Não deve ser possível cadastrar um novo aluguel caso já exista um aberto para o mesmo carro
+    const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
+      car_id
+    );
+
+    if (carUnavailable) {
+      throw new AppError("Car is unavailable");
+    }
+
+    // Não deve ser possível cadastrar um novo aluguel caso já exista um aberto para o mesmo usuário
+    const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(
+      user_id
+    );
+
+    if (rentalOpenToUser) {
+      throw new AppError("There's a rental in progress for user!");
+    }
+
+    // O aluguel deve ter duração mínima de 24 horas
+
+    const dateNow = this.dateProvider.dateNow();
+    const compare = this.dateProvider.compareInHours(
+      dateNow,
+      expected_return_date
+    );
+
+    if (compare < minimumHours) {
+      throw new AppError("Invalid return time!");
+    }
+    const rental = await this.rentalsRepository.create({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    return rental;
+  }
+}
+export { CreateRentalUseCase };
