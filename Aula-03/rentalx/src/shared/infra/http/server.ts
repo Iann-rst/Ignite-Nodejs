9381be7b39f6d7af17263126315978dee72dca04
Iann-rst <iannrst11@gmail.com>
2022-12-22 13:20:52 -0300

    feat: servidor

--- Aula-03/rentalx/src/shared/infra/http/server.ts
+++ Aula-03/rentalx/src/shared/infra/http/server.ts
@@ -1,36 +1,3 @@
-import express, { Request, Response, NextFunction } from "express";
-import swaggerUi from "swagger-ui-express";
-
-import "reflect-metadata";
-import "express-async-errors";
-import { AppError } from "@shared/errors/AppError";
-import createConnection from "@shared/infra/typeorm";
-
-import swaggerFile from "../../../swagger.json";
-import { router } from "./routes";
-
-import "@shared/container";
-
-createConnection();
-const app = express();
-app.use(express.json());
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-app.use(router);
-
-app.use(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        message: err.message,
-      });
-    }
-    return response.status(500).json({
-      message: `Internal server error - ${err.message}`,
-    });
-  }
-);
+import { app } from "./app";
 
 app.listen(3333, () => console.log("Server is running!"));
