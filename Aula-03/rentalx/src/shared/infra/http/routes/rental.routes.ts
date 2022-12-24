ff18e3d70dd2fea5cb8326fb9481974a5a03f635
Iann-rst <iannrst11@gmail.com>
2022-12-24 17:44:32 -0300

    feat: Rota para devolução do carro alugado

--- Aula-03/rentalx/src/shared/infra/http/routes/rental.routes.ts
+++ Aula-03/rentalx/src/shared/infra/http/routes/rental.routes.ts
@@ -1,12 +1,20 @@
 import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
+import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
 
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
+
 const rentalRoutes = Router();
 
 rentalRoutes.post("/", ensureAuthenticated, createRentalController.handle);
+rentalRoutes.post(
+  "/devolution/:id",
+  ensureAuthenticated,
+  devolutionRentalController.handle
+);
 
 export { rentalRoutes };
