ce06e9ceed5764eefe9080c1185ad2457424ddff
Iann-rst <iannrst11@gmail.com>
2022-12-24 17:50:28 -0300

    fix: consultar se tem aluguel com o carro ou user

--- Aula-03/rentalx/src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ Aula-03/rentalx/src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -18,13 +18,17 @@ class RentalsRepository implements IRentalsRepository {
   }
 
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
-    const openByCar = await this.repository.findOne({ car_id });
+    const openByCar = await this.repository.findOne({
+      where: { car_id, end_date: null },
+    });
 
     return openByCar;
   }
 
   async findOpenRentalByUser(user_id: string): Promise<Rental> {
-    const openByUser = await this.repository.findOne({ user_id });
+    const openByUser = await this.repository.findOne({
+      where: { user_id, end_date: null },
+    });
 
     return openByUser;
   }
