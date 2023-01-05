7d09c7781e4333303c63b8c2d31b5388fa8570ee
Iann-rst <iannrst11@gmail.com>
2023-01-05 17:49:53 -0300

    feat: Controller refresh token

--- Aula-03/rentalx/src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ Aula-03/rentalx/src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import auth from "@config/auth";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import { AppError } from "@shared/errors/AppError";
 
 interface IPayload {
@@ -15,6 +16,7 @@ export async function ensureAuthenticated(
 ) {
   // Bearer token
   const authHeader = request.headers.authorization;
+  const userTokensRepository = new UsersTokensRepository();
 
   if (!authHeader) {
     throw new AppError("Token missing", 401);
@@ -28,18 +30,20 @@ export async function ensureAuthenticated(
   try {
     const { sub: user_id } = verify(
       token,
-      "b59785b7da2085fdc46442df3728f5ad"
+      auth.secret_refresh_token
     ) as IPayload; // pega o id do usuário
 
-    const usersRepository = new UsersRepository();
-    const user = await usersRepository.findById(user_id);
+    const user = await userTokensRepository.findByUserIdAndRefreshToken(
+      user_id,
+      token
+    );
 
     if (!user) {
       throw new AppError("User does not exists!", 401);
     }
 
     request.user = {
-      id: user.id,
+      id: user_id,
     };
     return next();
   } catch {
