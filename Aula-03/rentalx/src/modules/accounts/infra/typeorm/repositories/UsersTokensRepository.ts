ce16ba438215e54f39475d76accf36defed4e825
Iann-rst <iannrst11@gmail.com>
2023-01-05 16:45:42 -0300

    feat: find Refresh_token_User, del refresh_token

--- Aula-03/rentalx/src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
+++ Aula-03/rentalx/src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -12,6 +12,21 @@ class UsersTokensRepository implements IUsersTokensRepository {
     this.repository = getRepository(UserTokens);
   }
 
+  async deleteById(id: string): Promise<void> {
+    await this.repository.delete(id);
+  }
+
+  async findByUserIdAndRefreshToken(
+    user_id: string,
+    token: string
+  ): Promise<UserTokens> {
+    const userTokens = await this.repository.findOne({
+      user_id,
+      refresh_token: token,
+    });
+    return userTokens;
+  }
+
   async create({
     expires_date,
     refresh_token,
