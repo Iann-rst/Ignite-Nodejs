f94d4bc026d07e830f34ce40b4f22bea2d4410cd
Iann-rst <iannrst11@gmail.com>
2023-01-07 15:56:05 -0300

    feat: Criando provider de e-mail

--- Aula-03/rentalx/src/shared/container/providers/index.ts
+++ Aula-03/rentalx/src/shared/container/providers/index.ts
@@ -3,7 +3,15 @@ import { container } from "tsyringe";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { DayjsDateProvider } from "@shared/container/providers/DateProvider/Implementations/DayjsDateProvider";
 
+import { IMailProvider } from "./MailProvider/IMailProvider";
+import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
+
 container.registerSingleton<IDateProvider>(
   "DayjsDateProvider",
   DayjsDateProvider
+);
+
+container.registerInstance<IMailProvider>(
+  "EtherealMailProvider",
+  new EtherealMailProvider()
 );
