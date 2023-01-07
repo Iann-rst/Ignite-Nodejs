19088e88a3678c251a1cbedfd3457ff3f01e2f4b
Iann-rst <iannrst11@gmail.com>
2023-01-07 18:33:11 -0300

    feat: Adicionando rota para set de senha

--- Aula-03/rentalx/src/shared/infra/http/routes/password.routes.ts
+++ Aula-03/rentalx/src/shared/infra/http/routes/password.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
+import { ResetPasswordUserController } from "@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController";
 import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController";
 
 const passwordRoute = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordUserController = new ResetPasswordUserController();
 
 passwordRoute.post("/forgot", sendForgotPasswordMailController.handle);
+passwordRoute.post("/reset", resetPasswordUserController.handle);
 
 export { passwordRoute };
