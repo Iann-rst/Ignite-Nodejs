e9c9e713167cb85507a14e14500b79df2fb66d35
Iann-rst <iannrst11@gmail.com>
2023-01-16 11:43:09 -0300

    fix: container (injeção de dependências)

--- Aula-03/rentalx/src/shared/container/providers/index.ts
+++ Aula-03/rentalx/src/shared/container/providers/index.ts
@@ -1,30 +1,3 @@
-import { container } from "tsyringe";
-
-import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
-import { DayjsDateProvider } from "@shared/container/providers/DateProvider/Implementations/DayjsDateProvider";
-import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
-
-import { IMailProvider } from "./MailProvider/IMailProvider";
-import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
-import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
-import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
-
-container.registerSingleton<IDateProvider>(
-  "DayjsDateProvider",
-  DayjsDateProvider
-);
-
-container.registerInstance<IMailProvider>(
-  "EtherealMailProvider",
-  new EtherealMailProvider()
-);
-
-const diskStorage = {
-  local: LocalStorageProvider,
-  s3: S3StorageProvider,
-};
-
-container.registerSingleton<IStorageProvider>(
-  "StorageProvider",
-  diskStorage[process.env.disk]
-);
+import "./DateProvider";
+import "./MailProvider";
+import "./StorageProvider";
